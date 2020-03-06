b4958c9f2d43b6398d4584a254b5bd68bda6167e
Michal <michal.malczyk@gmail.com>
2020-03-06 12:26:52 +0100

    init

---
+++ src/main/java/com/example/sample_spring_boot_demo/SampleSpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sample_spring_boot_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringBootDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sample_spring_boot_demo/SampleSpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sample_spring_boot_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
