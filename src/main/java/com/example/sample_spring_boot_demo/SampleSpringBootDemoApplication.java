bfa958a90096bea4de278d4a5ed266827b0bc7b2
Michal <michal.malczyk@gmail.com>
2020-03-09 13:35:44 +0100

    second commit

--- src/main/java/com/example/sample_spring_boot_demo/SampleSpringBootDemoApplication.java
+++ src/main/java/com/example/sample_spring_boot_demo/SampleSpringBootDemoApplication.java
@@ -1,13 +1,18 @@
 package com.example.sample_spring_boot_demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SampleSpringBootDemoApplication {
+public class SampleSpringBootDemoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleSpringBootDemoApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello world...");
+	}
 }
